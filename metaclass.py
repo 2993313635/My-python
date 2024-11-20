dfab6ed430ed9fea87f4eb3f4a3a29a35f3fa4fd
2993313635 <2993313635@qq.com>
2024-11-20 22:51:50 +0800

    Update metaclass.py

--- metaclass.py
+++ metaclass.py
@@ -24,3 +24,17 @@ Foo = type("Foo", (object,), {"__init__": init, "hello": hello, "cls_var": 10})
 foo = Foo("xxx")
 print(foo.hello())
 print(Foo.cls_var)
+
+
+class Author(type):
+    def __new__(mcs, name, bases, dict):
+        # 添加作者属性
+        dict["name"] = "xxx"
+        return super(Author, mcs).__new__(mcs, name, bases, dict)
+
+
+class Foo(object, metaclass=Author):
+    pass
+
+foo = Foo()
+print(foo.author)
